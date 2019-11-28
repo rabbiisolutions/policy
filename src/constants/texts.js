1dcb6e4da1c36e420acc8025c4cb2a72767142c5
pcf26536 <muffwaindan@gmail.com>
2019-11-28 11:58:24 +0300

    Add text constants and more icons [ch25]

---
+++ src/constants/texts.js
@@ -0,0 +1,15 @@
+const signUp = 'Sign Up';
+const menuTitle = 'Menu';
+const menuItems = ['Home', 'Our Services', 'About Us', 'FAQs', 'Contact Us', 'Trending'];
+const ourServices = ['TeeCha'];
+const hidden = 'hidden';
+const dot = '⦁ ';
+const signUpLong = 'Sign Up to Receive Our Payment Options';
+
+const texts = {
+  dot: dot, signUp: signUp, menuTitle: menuTitle, menuItems: menuItems, ourServices: ourServices,
+  hidden: hidden, signUpLong: signUpLong
+};
+
+export default texts;
+
