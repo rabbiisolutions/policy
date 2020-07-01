1aabaca02c6c8cea3759930e1db2e5bd188b5664
pcf26536 <muffwaindan@gmail.com>
2020-07-01 12:45:22 +0300

    update menu links [ch29]

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -1,8 +1,8 @@
 const signUp = 'Sign Up';
 const menuTitle = 'Menu';
-const menuItems = ['Home', 'Payment', 'Become a Tutor', 'About Us', 'FAQs', 'Trending'];
-const menuLinks = ['https://rabbii.co.ke', 'https://payment.rabbii.co.ke', 'https://tutor.rabbii.co.ke',
-  'https://about.rabbii.co.ke', 'https://faqs.rabbii.co.ke', 'https://trending.rabbii.co.ke'];
+const menuItems = ['Home', 'Become a Tutor', 'About Us', 'FAQs', 'Blog', 'Trending'];
+const menuLinks = ['https://rabbii.co.ke', 'https://tutor.rabbii.co.ke',
+  'https://about.rabbii.co.ke', 'https://faqs.rabbii.co.ke', 'https://blog.rabbii.co.ke', 'https://trending.rabbii.co.ke'];
 const ourServices = ['TeeCha'];
 const hidden = 'hidden';
 const dot = '⦁ ';
